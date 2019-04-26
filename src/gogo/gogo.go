70ce3d8d51ed407ca519c9233702fd00dedca76f
Dave Wade-Stein <dws@developintelligence.com>
2019-04-25 23:02:14 -0700

    refactor

--- src/gogo/gogo.go
+++
@@ -1,5 +0,0 @@
-package gogo
-
-func Hello() string {
-	return "Hello, world!"
-}
--- src/hello_test.go
+++ src/hello_test.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"testing"
-	"gogo"
-)
+import "testing"
 
 func TestHello(t *testing.T) {
-    if gogo.Hello() != "Hello, world!" {
+    if Hello() != "Hello, world!" {
 	t.Errorf("failed")
     }
 }
